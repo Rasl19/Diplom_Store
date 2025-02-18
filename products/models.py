1670f615b3453dd91e15a57cceb9b8cf7b5308a0
Nastyamind <rasul.nazmiev@mail.ru>
2025-02-18 20:33:06 +0300

    django_ORM_models_DB_done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategoryModels(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class ProductModel(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    image = models.ImageField(upload_to='products_images', verbose_name='Изображение')
+    category = models.ForeignKey(to=ProductCategoryModels, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return f'Название: {self.name} | Категория: {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- store/settings.py
+++ store/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
@@ -120,6 +120,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
